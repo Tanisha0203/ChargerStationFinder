6b254a6ae89ab28a81181bcfa85bac4788308b3f
harshutech <107917540+harshutech@users.noreply.github.com>
2025-06-10 18:06:45 +0530

    buildt

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from 'vuex'
 import axios from 'axios'
 
-const API_URL = 'http://localhost:5000/api'
+const API_URL = 'https://chargerstationfinder.onrender.com/'
 
 export default createStore({
   state: {
